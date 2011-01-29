3111aec2015a44ee6f298dd483d8087307e107a6
m0.interactive <m0.interactive@43b0d44d-17fb-f3be-cd75-413249885878>
2011-01-29 17:54:53 +0000

    Removing unwanted background scripts

--- trunk/client/js/background.js
+++ trunk/client/js/background.js
@@ -1,10 +1,3 @@
-// Extension constants for cross extension communication.
-const MAIN_EXTENSION_ID = 'ogpkflnbecojijgbndgmpnogclpeengc';
-const EXTENSION_IPC_TIMEOUT = 2000;
-
-// To validate the request if received.
-var alive = false;
-
 // Synchronously retrieve the current extension version.
 var version = 'NaN';
 var xhr = new XMLHttpRequest();
@@ -25,33 +18,3 @@ if (currVersion != prevVersion) {
   settings.version = currVersion;
 }
 
-
-/**
- * Send a request to the main extension.
- * @param {string} p the protocol to execute.
- * @param {object} m the message to execute.
- */
-function sendRequest(p, m) {
-  alive = false;
-  chrome.extension.sendRequest(MAIN_EXTENSION_ID, {protocol: p, message: m},
-    function (response) {
-      alive = true;
-    }
-  );
-  setTimeout(handleResponse, EXTENSION_IPC_TIMEOUT);
-}
-
-/**
- * If the main extension is not alive, we redirect the user to the error page
- * which will inform them that there is an error.
- */
-function handleResponse() {
-  if (!alive) {
-    openSingletonPage(chrome.extension.getURL('error.html'));
-  }
-}
-
-// On browser action clicked!
-chrome.browserAction.onClicked.addListener(function(tab) {
-  openSingletonPage(chrome.extension.getURL('admin.html'));
-});
