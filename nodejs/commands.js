f275e4a530a888f75d3512c676d11e9a0d63f61e
m0.interactive <m0.interactive@43b0d44d-17fb-f3be-cd75-413249885878>
2011-02-10 17:16:46 +0000

    Updated to allow communication with WebSocket client

--- nodejs/commands.js
+++ nodejs/commands.js
@@ -28,3 +28,58 @@ NicklistCommand.onMessage = function(server, conn, message) {
   server.send(conn, {id: conn.id, users: users}, NotificationCommand.NICKLIST);
 };
 
+JoinCommand= {};
+JoinCommand.onMessage = function(server, conn, message) {
+  var user = server.getUser(conn.id);
+  if (message && message != '') {
+    user.nick = message.nick;
+    user.url = message.url;
+  }
+
+  server.broadcast({nick: user.nick, id: conn.id}, NotificationCommand.JOIN);
+};
+
+LeaveCommand = {};
+LeaveCommand.onMessage = function(server, conn, message) {
+  var user = server.getUser(conn.id);
+
+  var users = server.getUsers();
+  for (var i in users) {
+    var u = users[i];
+    if (u.url == user.url) {
+      server.send(user.con, {nick: user.nick, url: user.url});
+    }
+  }
+
+  // Wipe out old URL.
+  user.url = null;
+};
+
+SayCommand = {};
+SayCommand.onMessage = function(server, conn, message) {
+  var user = server.getUser(conn.id);
+
+  var users = server.getUsers();
+  for (var i in users) {
+    var u = users[i];
+    if (u.url == user.url) {
+      server.send(u.con, {
+        command: 'SAY',
+        url: u.url,
+        wall: server.getWall(u.url),
+      });
+    }
+  }
+ 
+  // if (wall.message && wall.messages.length > 0) {
+  //server.broadcast({nick: user.nick, id: conn.id, message: message},
+ //                 NotificationCommand.MSG);
+};
+
+VisitCommand = {};
+VisitCommand.onMessage = function(server, conn, message) {
+  var user = server.getUser(conn.id);
+  server.broadcast({nick: user.nick, id: conn.id, message: message},
+                   NotificationCommand.MSG);
+};
+
